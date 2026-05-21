6a42957540a48186ad6e76e2134e0469458e4956
MannyTheBubby <usedforjunk11@gmail.com>
2026-05-21 11:33:44 -0400

    added helper scripts

--- image_script.py
+++ image_script.py
@@ -8,7 +8,7 @@ if os.path.exists(libdir):
     sys.path.append(libdir)
 
 import logging
-from waveshare_epd import epd2in9bc
+import epd2in9bc
 import time
 from PIL import Image,ImageDraw,ImageFont
 import traceback
